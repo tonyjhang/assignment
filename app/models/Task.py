bf91118247c65686c632da11811bd550b22d666a
Tony Jhang <tony40815@gmail.com>
2021-07-17 14:44:12 +0800

    feat(api): add task api

--- app/models/Task.py
+++ app/models/Task.py
@@ -2,12 +2,18 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
 class Task(Base):
     __tablename__ = 'task'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(200))
-    status = Column(Integer)
+    status = Column(Integer, default=0)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'status': self.status
+        }
 
 def create(engine):
     Base.metadata.create_all(engine)
